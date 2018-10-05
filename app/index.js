3032eda39824529572f17ca41a7d334155d39b87
Dylan Williams <dylan.williams@gatewayblend.com>
2018-10-04 22:52:45 -0500

    Create the Popular component

--- app/index.js
+++ app/index.js
@@ -2,15 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
-class App extends React.Component {
-    render () {
-        return (
-            <div>
-            Another test
-            </div>
-        )
-    }
-}
+var App = require('./components/App.js')
+
 
 ReactDOM.render(
     <App />,
